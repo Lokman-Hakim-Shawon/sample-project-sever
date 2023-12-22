5f7d289bc311eb5cf3915eb5ddee33bda4bb3f56
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-12-22 16:00:58 +0600

    update command

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const database = client.db("sample-project-server").collection("task");
     app.post('/task',async(req,res)=>{
         const newuser=req.body
@@ -52,8 +52,8 @@ async function run() {
       res.send(result)
     })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
