b385e61a8cb7da6fcda720a144a723f55d75877b
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-12-22 15:27:56 +0600

    update data

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // iLJW93Z9vDNrcyU1
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.trqhxan.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,6 +34,22 @@ async function run() {
     app.get('/task',async(req,res)=>{
         const result = await database.find().toArray();
         res.send(result)
+    })
+    app.put('/task/:id',async(req,res)=>{
+      const id=req.params.id
+      const filter={_id:new ObjectId(id)}
+      const updaterequest=req.body
+      const option={upsert:true}
+      const update={
+        $set:{
+          title:updaterequest.title,
+          img:updaterequest.img,
+          deadline:updaterequest.deadline,
+          des:updaterequest.des
+        }
+      }
+      const result= await database.updateOne(filter,update,option)
+      res.send(result)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
